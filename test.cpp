7da351bd8c35e35eb5d7f362165c37b7a64d978d
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2016-08-20 01:29:26 +0100

    Read metadata into object

--- test.cpp
+++ test.cpp
@@ -1,36 +1,89 @@
 #include <iostream>
 #include <sqlite3.h>
+#include <string>
+#include <cstring>
+#include <map>
 using namespace std;
 
-static int callback(void *NotUsed, int argc, char **argv, char **azColName)
+class MBTileReader
 {
-	for(int i=0; i<argc; i++){
-		cout << azColName[i] << "=" << (argv[i] ? argv[i] : "NULL") << endl;
-	}
-	cout << endl;
-	return 0;
-}
+public:
+	MBTileReader(const char *filename);
+	virtual ~MBTileReader();
 
-int main(int argc, char **argv){
-	sqlite3 *db = NULL;
+	string GetMetadata(const char *metaField);
 
-	int rc = sqlite3_open("cairo_egypt.mbtiles", &db);
-	if(rc){
+protected:
+	sqlite3 *db;
+	map<string, string> metadata;
+
+	static int MetadataCallbackStatic(void *obj, int argc, char **argv, char **azColName);
+	int MetadataCallback(int argc, char **argv, char **azColName);
+
+};
+
+MBTileReader::MBTileReader(const char *filename)
+{
+	this->db = NULL;
+
+	int status = sqlite3_open(filename, &this->db);
+	if(status){
 		cout << "Error opening database" << endl;
-		sqlite3_close(db);
-		return -1;
+		sqlite3_close(this->db);
+		return;
 	}
 
 	char *zErrMsg = 0;
-	rc = sqlite3_exec(db, "SELECT * FROM metadata;", callback, 0, &zErrMsg);
-	if( rc!=SQLITE_OK )
+	status = sqlite3_exec(this->db, "SELECT * FROM metadata;", this->MetadataCallbackStatic, this, &zErrMsg);
+	if( status!=SQLITE_OK )
 	{
 		cout << "SQL error " << zErrMsg << endl;
 		sqlite3_free(zErrMsg);
-		return -1;
+		return;
 	}
+}
 
-	sqlite3_close(db);
+MBTileReader::~MBTileReader()
+{
+	sqlite3_close(this->db);
+}
+
+int MBTileReader::MetadataCallbackStatic(void *obj, int argc, char **argv, char **azColName)
+{
+	return ((MBTileReader*)obj)->MetadataCallback(argc, argv, azColName);
+}
+
+int MBTileReader::MetadataCallback(int argc, char **argv, char **azColName)
+{
+	string name, value;
+	for(int i=0; i<argc; i++){
+		//cout << azColName[i] << "=" << (argv[i] ? argv[i] : "NULL") << endl;
+		if (strncmp(azColName[i], "name", 5) == 0)
+			name = (argv[i] ? argv[i] : "NULL");
+		if (strncmp(azColName[i], "value", 6) == 0)
+			value = (argv[i] ? argv[i] : "NULL");		
+	}
+	if (name.size() > 0)
+		this->metadata[name] = value;
+	//cout << endl;
 	return 0;
+}
+
+string MBTileReader::GetMetadata(const char *metaField)
+{
+	return metadata[metaField];
+}
+
+int main(int argc, char **argv)
+{
+	class MBTileReader mbTileReader("cairo_egypt.mbtiles");	
+	
+	cout << "name:" << mbTileReader.GetMetadata("name") << endl;
+	cout << "type:" << mbTileReader.GetMetadata("type") << endl;
+	cout << "version:" << mbTileReader.GetMetadata("version") << endl;
+	cout << "description:" << mbTileReader.GetMetadata("description") << endl;
+	cout << "format:" << mbTileReader.GetMetadata("format") << endl;
+	cout << "bounds:" << mbTileReader.GetMetadata("bounds") << endl;
+
 }
 
