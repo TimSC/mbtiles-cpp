b0e608c06a2d515667b219e5ab7fba8ca99d9bc6
serinox <jason@freelancersunite.net>
2017-06-03 09:53:08 -0400

    fix latitude calculations.

--- Source/VectorTile.cpp
+++ Source/VectorTile.cpp
@@ -264,25 +264,31 @@ void DecodeVectorTile::DecodeGeometry(const ::vector_tile::Tile_Feature &feature
 // *********************************************
 
 // https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#C.2FC.2B.2B
-int long2tilex(double lon, int z) 
-{ 
-	return (int)(floor((lon + 180.0) / 360.0 * pow(2.0, z))); 
+int long2tilex(double lon, int z)
+{
+	return (int)(floor((lon + 180.0) / 360.0 * pow(2.0, z)));
 }
 
 int lat2tiley(double lat, int z)
-{ 
-	return (int)(floor((1.0 - log( tan(lat * M_PI/180.0) + 1.0 / cos(lat * M_PI/180.0)) / M_PI) / 2.0 * pow(2.0, z))); 
+{
+	int y = (int)(floor((1.0 - log( tan(lat * M_PI/180.0) + 1.0 / cos(lat * M_PI/180.0)) / M_PI) / 2.0 * pow(2.0, z)));
+	int ymax  = 1 << z;
+	y = ymax - y - 1;
+	return y;
 }
 
-double tilex2long(int x, int z) 
+double tilex2long(int x, int z)
 {
 	return x / pow(2.0, z) * 360.0 - 180;
 }
 
-double tiley2lat(int y, int z) 
+double tiley2lat(int y, int z)
 {
-	double n = M_PI - 2.0 * M_PI * y / pow(2.0, z);
-	return 180.0 / M_PI * atan(0.5 * (exp(n) - exp(-n)));
+	double n = pow(2.0,z);
+	int ymax  = 1 << z;
+	y = ymax - y - 1;
+	double latRad = atan(sinh(M_PI*(1-(2*y/n))));
+	return 180.0 / M_PI * latRad;
 }
 
 // ***********************************************
